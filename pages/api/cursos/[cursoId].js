c82c4c0bdf221ccd4ec5e6d063aa909ae91fb001
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2022-07-05 12:54:30 -0300

    03:03 - Criando APIs

---
+++ pages/api/cursos/[cursoId].js
@@ -0,0 +1,17 @@
+const meusCursos = [
+    { id: 1, nome: 'React.js' },
+    { id: 2, nome: 'Next.js' }
+]
+
+export default function Cursos(request, response) {
+    const id = request.query.cursoId
+
+    const curso = meusCursos.find(curso => curso.id == id)
+
+    if (curso) {
+
+        response.status(200).json(curso)
+    } else {
+        response.status(404).json({ message: 'Curso não encontrado' })
+    }
+}
--- pages/index.js
+++ pages/index.js
@@ -17,8 +17,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Hello Wold
         </p>
 
         <div className={styles.grid}>
